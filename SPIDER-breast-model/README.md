6cf412746bc16c394394b37395876d9e1b240593
JACHUPOL <agarplaypl@gmail.com>
2025-10-09 17:46:33 +0200

    SPIDER-breast-model

---
+++ SPIDER-breast-model/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eeca474ed59d6a9a8eb0c7ddc7d17a775fbb8dafdbe76b12e5c9283040b570c5
+size 3940
---
+++ SPIDER-breast-model/configuration_spider.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98536973b869a8236716ae386388e3ea412cd142d4e44fd4f3a7aa33cc7480f0
+size 2724
